5835716fc5bbf643c47882dca92c02bf0d2b5141
douglas1850 <douglas12219@gmail.com>
2018-11-28 23:38:36 -0600

    Created header for generic info, started doing some calculated info

--- src/Main.java
+++ src/Main.java
@@ -13,8 +13,26 @@ public class Main {
             whichOne(args[i], args[i+1]);
         }
 
-        System.out.println("File name: " + fileName + "\nCache Size: " + cacheSize + "\nBlock Size: "
-        + blockSize + "\nAssociativity: " + associativity + "\nReplacement Policy: " + replacementPolicy);
+        //System.out.println("File name: " + fileName + "\nCache Size: " + cacheSize + "\nBlock Size: "
+        //+ blockSize + "\nAssociativity: " + associativity + "\nReplacement Policy: " + replacementPolicy);
+
+        System.out.println("Cache Simulator CS 3853 Fall 2018 – Group #16");
+        System.out.println("\nTrace File: " + fileName);
+        System.out.println("\n----- Generic -----");
+        System.out.println("Cache Size: " + cacheSize + " KB");
+        System.out.println("Block Size: " + blockSize + " bytes");
+        System.out.println("Associativity: " + associativity);
+        System.out.println("Policy: " + replacementPolicy);
+
+
+        System.out.println("\n----- Calculated Values ----- ");
+        System.out.println("Total #Blocks: " + (int)(Math.pow(2, Integer.valueOf(blockSize))/1000) + " KB"); //2^block size KB
+        System.out.println("Tag Size: " + " bits"); //# of address bits minus the # of index bits, minus the # of offset bits (within the cache block).
+        System.out.println("Index Size: *** bits, Total Indices: ***");
+        System.out.println("Implementation Memory Size: ***");
+
+        System.out.println("\n----- Results -----");
+        System.out.println("Cache Hit Rate: *** %");
     }
 
 
