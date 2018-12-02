147a49f21aef149411acc22e995dfd8cd15973ef
cullen-lowe <cullen.lowe@yahoo.com>
2018-12-02 15:17:39 -0600

    two way and four way

---
+++ src/FourWayObject.java
@@ -0,0 +1,78 @@
+public class FourWayObject {
+
+    int value; //dirty or valid bit
+    String tag;
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    int value2; //dirty or valid bit
+    String tag2;
+
+    public void setValue2(int value) {
+        this.value2 = value;
+    }
+
+    public void setTag2(String tag) {
+        this.tag2 = tag;
+    }
+
+    public int getValue2() {
+        return value2;
+    }
+
+    public String getTag2() {
+        return tag2;
+    }
+
+    int value3; //dirty or valid bit
+    String tag3;
+
+    public void setValue3(int value) {
+        this.value3 = value;
+    }
+
+    public void setTag3(String tag) {
+        this.tag3 = tag;
+    }
+
+    public int getValue3() {
+        return value3;
+    }
+
+    public String getTag3() {
+        return tag3;
+    }
+
+    int value4; //dirty or valid bit
+    String tag4;
+
+    public void setValue4(int value) {
+        this.value4 = value;
+    }
+
+    public void setTag4(String tag) {
+        this.tag4 = tag;
+    }
+
+    public int getValue4() {
+        return value4;
+    }
+
+    public String getTag4() {
+        return tag4;
+    }
+}
